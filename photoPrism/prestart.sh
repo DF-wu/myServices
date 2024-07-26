ca86fbaba56cfaa21a56204beab260560cadaf40
D.F. <df@dfder.tw>
2024-07-26 16:16:25 +0800

    fix: folder relative path

--- photoPrism/prestart.sh
+++ photoPrism/prestart.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # do it before start photoPrism
 
-rm -rf ./built-ui-componets
+sudo rm -rf ./built-ui-componets
 mkdir ./built-ui-componets
-rm -rf ./photoprism_src
+sudo rm -rf ./photoprism_src
 git clone --branch release https://github.com/photoprism/photoprism.git ./photoprism_src
 pushd photoprism_src
 sed -i '/name: "Test",/a\  sponsor: true,' ./frontend/src/common/config.js
@@ -12,6 +12,7 @@ docker run --rm \
   -v ./photoprism_src:/workspace \
   --entrypoint /bin/sh \
   node:lts-alpine -c "cd /workspace/frontend && npm i && npm run build"
+sudo rm -rf /mnt/appdata/photoprism/df-built-ui-componets
 cp -r ./photoprism_src/assets/static/build /mnt/appdata/photoprism/df-built-ui-componets 
 
 
