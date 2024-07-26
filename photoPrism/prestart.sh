a608864fda3918fe8add4b08665e8bc9bd5f40ab
D.F. <df@dfder.tw>
2024-07-26 15:56:19 +0800

    fix: npm whole project to keep the generated files.

--- photoPrism/prestart.sh
+++ photoPrism/prestart.sh
@@ -7,11 +7,11 @@ rm -r ./photoprism_src
 git clone --branch release https://github.com/photoprism/photoprism.git ./photoprism_src
 pushd photoprism_src
 sed -i '/name: "Test",/a\  sponsor: true,' ./frontend/src/common/config.js
-docker run --rm \
-  -v ./frontend:/workspace \
-  --entrypoint /bin/sh \
-  node:lts-alpine -c "cd /workspace && npm i && npm run build"
 popd
+docker run --rm \
+  -v ./photoprism_src:/workspace \
+  --entrypoint /bin/sh \
+  node:lts-alpine -c "cd /workspace/frontend && npm i && npm run build"
 cp -r ./photoprism_src/assets/static/build ./built-ui-componets
 
 
