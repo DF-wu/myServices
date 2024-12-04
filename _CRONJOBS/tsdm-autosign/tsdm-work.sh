4ae640a89718808f8c1ffbbfda7c577b3a11f9e6
D.F. <df@dfder.tw>
2024-12-04 16:30:14 +0800

    feat: add logs

--- _CRONJOBS/tsdm-autosign/tsdm-work.sh
+++ _CRONJOBS/tsdm-autosign/tsdm-work.sh
@@ -4,6 +4,6 @@
 # desc: tsdm自动签到脚本。 
 # 修改自 https://github.com/trojblue/TSDM-coin-farmer 和 https://github.com/trojblue/TSDM-coin-farmer/pull/20
 # 抽取其中重點部分，並將其修改為適合local執行的腳本。
-docker run --rm -it -v "$(pwd):/app" python:3.6 /bin/bash -c "pip install -r /app/requirements.txt && cd /app && python /app/SCF_sign.py && python /app/SCF_work.py"
+docker run --rm -it -v "$(pwd):/app" python:3.6 /bin/bash -c "pip install -r /app/requirements.txt && cd /app && python /app/SCF_sign.py && python /app/SCF_work.py" > output.log 2>&1
 
 
