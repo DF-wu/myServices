77be2b84b14ed012d1a89d2c500db9fdfd3f5213
D.F. <df@dfder.tw>
2024-12-06 01:58:17 +0800

    feat: new linux.do auto read

---
+++ linux.do-bot/cron.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#设置为中文
+export LANG=zh_CN.UTF-8
+export LC_ALL=zh_CN.UTF-8
+# 获取当前工作目录
+WORKDIR=$(dirname $(readlink -f $0))
+
+# 进入工作目录
+cd $WORKDIR
+
+# 停止 Docker Compose
+/usr/local/bin/docker-compose down --remove-orphans --volumes
+
+# 重新启动 Docker Compose
+/usr/local/bin/docker-compose up -d >> ./cron.log 2>&1
+
+# 等待60分钟
+sleep 60m
+/usr/local/bin/docker-compose logs >> ./cron.log 2>&1
+
+# 停止 Docker Compose
+/usr/local/bin/docker-compose down --remove-orphans --volumes >> ./cron.log 2>&1
